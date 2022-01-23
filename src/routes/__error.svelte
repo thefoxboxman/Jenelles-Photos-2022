a7d83245f182eac27bc3c17b3ba6030efeab01b4
John De Costa <thefoxboxman@users.noreply.github.com>
2022-01-23 20:43:04 +1000

    added a 404 page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,28 @@
+
+
+<div id="errormessage">
+<h1>404</h1>
+<h3>Sorry</h3>
+<p>“We couldn’t find the page you were looking for.”</p>
+<a class="nav-link"  href="/">Return to Home</a>
+</div>
+
+<style>
+#errormessage {
+  margin: 10rem auto;
+  text-align: center;
+}
+
+.nav-link {
+    text-decoration: none;
+    color: orange;
+
+    border: solid 0.1rem orange;
+    border-radius: 0.2rem;
+    font-family: Roboto, "Open Sans", "Helvetica Neue", sans-serif;
+    font-size: 1.75rem;
+    padding: 0.1rem 0.3rem;
+    margin: 0.2rem;
+    text-transform: capitalize;
+  }
+</style>
