5df033ea7f1022021522ea079156c7a7948c3b2e
John De Costa <thefoxboxman@users.noreply.github.com>
2022-01-28 20:03:46 +1000

    fixing plain index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,43 +1,20 @@
 
 
-
-<script context="module">
-  export async function load({fetch}) {
-    try {
-      const res = await fetch('/photos/allphotos.json')
-      const data = await res.json()
-      return {
-        props: data
-      }
-    } catch (err) {
-      console.log('500:', err)
-    }
-  }
-</script>
-
-<script>
-  
-  import PhotoRoll from '$lib/PhotoRoll.svelte'
-
-  export let photos
-  
-</script>
-
 <svelte:head>
-  <title>Photo Roll 2022</title>
+  <title>Home</title>
 </svelte:head>
 
 
-
-<h1>is this working</h1>
-
+<h1>Welcome to Jenelle's Photos 2022</h1>
 
 
 
 
 
 <style>
- h1{color: bisque}
-  
+  h1{
+    color: bisque
+  }
 </style>
+
 
