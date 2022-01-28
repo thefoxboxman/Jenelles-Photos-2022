a16d149ee875cbf20044341b493aff15dfe97f9a
John De Costa <thefoxboxman@users.noreply.github.com>
2022-01-28 19:38:01 +1000

    copied index.svelte to src not in routes

---
+++ src/index.svelte
@@ -0,0 +1,43 @@
+
+
+
+<script context="module">
+  export async function load({fetch}) {
+    try {
+      const res = await fetch('/photos/allphotos.json')
+      const data = await res.json()
+      return {
+        props: data
+      }
+    } catch (err) {
+      console.log('500:', err)
+    }
+  }
+</script>
+
+<script>
+  
+  import PhotoRoll from '$lib/PhotoRoll.svelte'
+
+  export let photos
+  
+</script>
+
+<svelte:head>
+  <title>Photo Roll 2022</title>
+</svelte:head>
+
+
+
+<PhotoRoll {photos} />
+
+
+<h1>is this working</h1>
+
+
+
+<style>
+ 
+  
+</style>
+
