dc5c749f4e2f1febb6ead88632437b559259e16c
John De Costa <thefoxboxman@users.noreply.github.com>
2022-01-29 20:43:38 +1000

    added landing page with image background

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,39 @@
-
-
 <svelte:head>
   <title>Home</title>
 </svelte:head>
 
+<div class="main">
+  <h1>Welcome to Jenelle's Photos 2022</h1>
 
-<h1>Welcome to Jenelle's Photos 2022</h1>
+  <p>You can check out the results of my, self challenge, to take a photo a day. </p>
+  <p>Using the navigation bar at the top the page you can choose to see the entire photo roll by
+    clicking the "Roll" button...
+  </p>
+  <p>...or you can see images I have tagged to a range of catergories such as flowers, birds etc.</p>
+<p>I am also writing a blog to give you some insights into my thoughts and practice
+  in achieving the final images.
+</p>
+<p>Thank you for taking the time to visit my site....Enjoy!</p>
 
-
-
-
+</div>
 
 <style>
-  h1{
-    color: bisque;
+  .main {
+    background-color: black;
+    background-image: url(/Scape-P1430983.jpg);
+    background-size: cover;
+    background-position: center;
+    padding: 5rem;
+  }
+  h1 {
+    font-size: 3rem;
+    color: whitesmoke;
     text-align: center;
   }
+
+   p {
+    color: whitesmoke;
+    text-align: center;
+    margin: 1rem
+  }
 </style>
-
-
